beb6c1105360a3ac810b0e370d0ce01ddd7f81e5
ratikantabaral <kanharatikanta@gmail.com>
2025-05-18 09:19:57 +0530

    error

--- src/main/java/com/javatest/javatest/Respositary/RespositaryTest.java
+++ src/main/java/com/javatest/javatest/Respositary/RespositaryTest.java
@@ -14,7 +14,7 @@ import java.util.Optional;
 import java.util.function.Function;
 
 @Repository
-public interface RespositaryTest extends JpaRepository<Test,Long> {
+public interface RespositaryTest extend JpaRepository<Test,Long> {
 
 
 }
